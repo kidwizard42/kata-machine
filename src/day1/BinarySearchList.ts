79944c13030fe4872bcbbd0130ac7bb7f5c3deab
Darwood Antoine <darwooda@gmail.com>
2023-07-01 13:50:36 -0400

    BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,30 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+    do {
+        let m = Math.floor(lo + (hi - lo) / 2);
+        if (haystack[m] === needle) {
+            return true;
+        } else if (haystack[m] < needle) {
+            lo = m + 1;
+        } else {
+            hi = m;
+        }
+    } while (lo < hi);
+    return false;
 
+    // Darwood Version    // MAYBE TEST ON LEETCODE??
+    // let lo = 0;
+    // let hi = haystack.length;
+    // while (lo < hi) {
+    //     let m = Math.floor((lo + hi) / 2);
+    //     if (haystack[m] === needle) {
+    //         return true;
+    //     } else if (haystack[m] < needle) {
+    //         lo = m + 1;
+    //     } else {
+    //         hi = m - 1;
+    //     }
+    // }
+    // return false;
 }
