edc549bb4b35de3894cf5749316d7fa9ac151097
Darwood Antoine <darwooda@gmail.com>
2023-07-02 18:43:20 -0400

    2CryBall prblm solved w my code. (pseudo from him)

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let lastSafePoint: number = 0;
+    let squareRoot: number = Math.floor(Math.sqrt(breaks.length));
+    let jumpNum = squareRoot;
 
+    while (true) {
+        if (breaks[jumpNum] || breaks[jumpNum] === undefined) {
+            break;
+        } else {
+            lastSafePoint = jumpNum;
+            jumpNum += squareRoot;
+        }
+    }
+
+    for (let i = lastSafePoint; i <= jumpNum; i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
